b77b34c1ca477dc51535f0afdf7c5c8cbff0747e
Glen Ernstrom <otterbrain@protonmail.com>
2024-09-27 22:20:41 -0400

    home-manager updates

--- flake.nix
+++ flake.nix
@@ -4,24 +4,32 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = {self, nixpkgs, nixpkgs-unstable, ...}:
+  outputs = {self, nixpkgs, nixpkgs-unstable, home-manager,...}:
     let
        system = "x86_64-linux"; 
        lib = nixpkgs.lib;
-       pkgs = nixpkgs.legacyPackage.${system};
+       pkgs = nixpkgs.legacyPackages.${system};
        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
     in {
       nixosConfigurations = {
-      rosie = lib.nixosSystem {
+       rosie = lib.nixosSystem {
            inherit system;
            modules = [ ./configuration.nix ];
            specialArgs = {
              inherit pkgs-unstable;
-      };
-     };
+            };
+          };
+        };
+      homeConfigurations = {
+       ernstrom = home-manager.lib.homeManagerConfiguration {
+           inherit pkgs;
+           modules = [ ./home.nix ];
+     }; 
    };
   };
 }
